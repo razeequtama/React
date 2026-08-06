6638a1d5d991be40ba1b604c616070238e649443
Raziq <rabbaniu74m4@gmail.com>
2026-08-06 14:31:57 +0700

    Add: useState practice

---
+++ 7. useState/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Counter from "./components/Counter";
+
+function App()
+{
+    return <Counter />
+}
+
+export default App;
---
+++ 7. useState/src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+
+function Counter()
+{
+    let [counter, setCounter] = useState(0);
+
+    function increase()
+    {
+        setCounter(counter => counter += 1);
+    }
+
+    return(
+        <>
+            <button onClick={increase}>Counter is {counter}</button>
+        </>
+    );
+};
+
+export default Counter;
